f3ed5ab79919c93bded52e7c65a4451c6405c228
VictorCottar <victorcottarms@outlook.com>
2024-07-26 14:33:30 -0300

    Teste api login e registro

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,12 @@
-import { FiEye, FiEyeOff } from 'react-icons/fi';
-import { useState } from 'react';
-import ModalCadastro from './ModalCadastro';
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { useState } from "react";
+import ModalCadastro from "./ModalCadastro";
 
 export default function Formulario() {
+  const [password, setPassword] = useState(false);
+  const [email, setEmail] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false); // Adiciona o estado isModalOpen
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -13,30 +15,84 @@ export default function Formulario() {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  const handleLogin = async () => {
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    response.status != 400 ? console.log("logado com sucesso", data) : console.log("Erro ao logar", data);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleLogin();
+  };
+
   return (
     <>
-      <form action="" className="flex flex-col justify-center p-8 max-w-[550px] max-h-[550px] space-y-6 border-2 border-r-4 border-b-4 border-orange-500 rounded-lg shadow-shape">
-        <div className='flex items-center justify-center text-center flex-wrap'>
-          <h2 className='text-orange-500 text-2xl font-medium'>
-            Faça seu login ou <button type="button" onClick={openModal} className='underline'>cadastre-se</button>!
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center p-8 max-w-[550px] max-h-[550px] space-y-6 border-2 border-r-4 border-b-4 border-orange-500 rounded-lg shadow-shape"
+      >
+        <div className="flex items-center justify-center text-center flex-wrap">
+          <h2 className="text-orange-500 text-2xl font-medium">
+            Faça seu login ou{" "}
+            <button type="button" onClick={openModal} className="underline">
+              cadastre-se
+            </button>
+            !
           </h2>
         </div>
         <div className="flex flex-col items-center space-y-4">
-          <div className='flex flex-col space-y-2 w-11/12'>
-            <label className="text-3xl text-orange-500 font-light" htmlFor="email">Email</label>
-            <input className="p-3 rounded-lg text-orange-500 shadow-shape focus:outline-none" type="email" placeholder="Digite seu email" />
+          <div className="flex flex-col space-y-2 w-11/12">
+            <label
+              className="text-3xl text-orange-500 font-light"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="p-3 rounded-lg text-orange-500 shadow-shape focus:outline-none"
+              type="email"
+              placeholder="Digite seu email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
 
-          <div className='flex flex-col space-y-2 w-11/12'>
-            <label className="text-3xl text-orange-500 font-light" htmlFor="senha">Senha</label>
+          <div className="flex flex-col space-y-2 w-11/12">
+            <label
+              className="text-3xl text-orange-500 font-light"
+              htmlFor="senha"
+            >
+              Senha
+            </label>
             <div className="relative">
-              <input className="p-3 rounded-lg text-orange-500 shadow-shape focus:outline-none w-full" type={showPassword ? "text" : "password"} placeholder="Digite sua senha" />
-              <button type="button" onClick={togglePasswordVisibility} className="absolute inset-y-0 right-0 pr-3 flex items-center text-orange-500">
+              <input
+                className="p-3 rounded-lg text-orange-500 shadow-shape focus:outline-none w-full"
+                type={showPassword ? "text" : "password"}
+                placeholder="Digite sua senha"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center text-orange-500"
+              >
                 {showPassword ? <FiEyeOff /> : <FiEye />}
               </button>
             </div>
           </div>
-          <button type="submit" className="bg-orange-500 p-3 rounded-lg shadow-shape w-11/12 hover:bg-orange-300">
+          <button
+            type="submit"
+            className="bg-orange-500 p-3 rounded-lg shadow-shape w-11/12 hover:bg-orange-300"
+          >
             Login
           </button>
         </div>
--- src/components/ModalCadastro.jsx
+++ src/components/ModalCadastro.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { RiCloseFill } from "react-icons/ri";
 
 ModalCadastro.propTypes = {
@@ -7,6 +8,24 @@ ModalCadastro.propTypes = {
 };
 
 export default function ModalCadastro({ isOpen, onClose }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleRegister = async (event) => {
+      event.preventDefault();
+
+      const response = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      response.status != 400 ? window.alert("Registrado com sucesso, feche para fazer o login", data) : window.alert("Cadastro já existente", data);
+    
+  };
+  
   if (!isOpen) return null;
 
   if (isOpen) {
@@ -14,7 +33,7 @@ export default function ModalCadastro({ isOpen, onClose }) {
       <div className="flex items-center justify-center h-screen w-screen">
         <div className="fixed inset-0 bg-black/90 flex justify-center items-center m-auto">
           <form
-            action=""
+            onSubmit={handleRegister}
             className="flex flex-col justify-center p-8 m-auto max-w-[550px] max-h-[550px] space-y-6 border-2 border-r-4 border-b-4 border-orange-500 bg-neutral-700 rounded-lg shadow-shape"
           >
             <div className="flex justify-end">
@@ -39,6 +58,8 @@ export default function ModalCadastro({ isOpen, onClose }) {
                   className="p-3 rounded-lg  text-orange-500 shadow-shape focus:outline-none"
                   type="email"
                   placeholder="Digite seu email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
                 />
               </div>
               <div className="flex flex-col space-y-2 w-11/12">
@@ -53,6 +74,8 @@ export default function ModalCadastro({ isOpen, onClose }) {
                   className="p-3 rounded-lg text-orange-500 shadow-shape focus:outline-none w-full"
                   type="password"
                   placeholder="Digite sua senha"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
                 />
               </div>
               <button
